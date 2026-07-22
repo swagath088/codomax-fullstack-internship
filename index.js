5948dc05684022b27a5a789d754f517d8c4b3904
Gaddam Swagath <gaddamswagath088@gmail.com>
2026-07-22 15:38:01 +0530

    Day 6: Added Blog API using JavaScript array

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = 3000;
 
 // Middleware
 app.use(express.json());
 
-// GET Route
+// JavaScript array to store blogs
+const blogs = [];
+
+// Home Route
 app.get("/", (req, res) => {
-    res.send("Welcome to Codomax Express Server!");
+    res.send("Welcome to Codomax Blog API!");
 });
 
-// GET Route - Blog
-app.get("/blog", (req, res) => {
-    res.json({
-        message: "This is the Blog Page"
-    });
+// Get All Blogs
+app.get("/blogs", (req, res) => {
+    res.json(blogs);
 });
 
-// POST Route
-app.post("/blog", (req, res) => {
+// Add Blog API
+app.post("/blogs", (req, res) => {
 
     const { title, author } = req.body;
 
-    res.json({
+    if (!title || !author) {
+        return res.status(400).json({
+            success: false,
+            message: "Title and Author are required."
+        });
+    }
+
+    const newBlog = {
+        id: blogs.length + 1,
+        title,
+        author
+    };
+
+    blogs.push(newBlog);
+
+    res.status(201).json({
         success: true,
-        message: "Blog added successfully!",
-        blog: {
-            title,
-            author
-        }
+        message: "Blog created successfully!",
+        blog: newBlog
     });
 
 });
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
